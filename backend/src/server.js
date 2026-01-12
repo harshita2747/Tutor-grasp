948b4ed7eaf16f7bd36f2dc3630cfdf18d9bd4dc
harshita2747 <harshi2747@gmail.com>
2026-01-12 16:06:48 +0530

    routes and controller is added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,13 @@
-import app from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import app from "./app.js";
 
 dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
 connectDB();
 
-
-const PORT =process.env.PORT || 5000;
-
-app.listen(PORT,() => {
-    console.log(`server is running ${PORT}`);
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
